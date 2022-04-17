f82d6c92528fd25c9d893287cdafca7bcdd5a851
NicholasRomanov <nicholasromanov51@gmail.com>
2022-04-17 13:28:52 +0700

    yes

---
+++ 1/1.3.js
@@ -0,0 +1,23 @@
+/**
+ * -Algoritma
+ * 1. Buat var r
+ * 2. Buat var pi
+ * 3. Isi var pi dengan 3.14
+ * 4. Buat var volume
+ * 5. Isi var volume dengan rumus pi kali r kali r kali r
+ * 6. Cetak volume
+ * 
+ * PSEUDOCODE
+ * STORE r WITH ANY NUMBER
+ * STORE pi WITH 3.14
+ * 
+ * SET volume WITH pi MULTIPLY r MULTIPLY r MULTIPLY r DIVIDE 2
+ * DISPLAY volume
+ */
+
+ var r = 5;
+ var phi;
+ phi = 3.14;
+ 
+ volume = (r*r*r)*phi/2;
+ console.log(volume)
