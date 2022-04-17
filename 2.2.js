6644c4504b619402353ee99704de2ecfa7fe1b19
NicholasRomanov <nicholasromanov51@gmail.com>
2022-04-17 11:28:21 +0700

    yes

---
+++ 2.2.js
@@ -0,0 +1,34 @@
+var minuman = "NGETEH";
+
+switch (minuman) {
+    
+    case "Sweet Tea":
+    console.log("You choose Sweet Tea, thanks")
+     break;
+   
+    case "Plain Tea":
+    console.log("You choose Plain Tea, thanks")
+     break;
+   
+    case "Boba":
+    console.log("You choose Boba, thanks")
+     break;
+    
+    case "Milk Coffee":
+    console.log("You choose Milk Coffee, thanks")
+     break;
+    
+    case "Choco Ice":
+    console.log("You choose Choco Ice, thanks")
+     break;
+    
+     default:
+    
+     if (minuman === "") {
+    console.log("Input minuman")
+    } else 
+    {
+    console.log("minuman is undefined")
+    }
+    break;
+}
