2f021e3c625c0a0a3a2db5498a152e497d60131a
NicholasRomanov <nicholasromanov51@gmail.com>
2022-04-17 11:18:44 +0700

    2.1

--- 2.1.js
+++ 2.1.js
@@ -8,15 +8,15 @@ var power = 75;
 {
     console.log("you are a Pro ");
 }
-else if (power <= 70 && power >50 ); 
+ else if (power <= 70 && power > 50 ); 
 {
     console.log("you are Normal ");
 }
-else if (power <= 50 && power > 30); 
+ else if (power <= 50 && power > 30); 
 {
     console.log("you are basic ");
 }
-else if (power <= 30); 
+ else if (power <= 30); 
 {
     console.log("you are a Beginner "); 
 }
