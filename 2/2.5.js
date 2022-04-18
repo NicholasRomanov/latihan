ce276427714ff0f5a3c86cdfaac4abcd92ef1757
NicholasRomanov <nicholasromanov51@gmail.com>
2022-04-18 16:12:28 +0700

    yes

---
+++ 2/2.5.js
@@ -0,0 +1,13 @@
+var input = 5;
+var temp = "";
+
+for (var i = 0; i < input; i++) {
+    if (i % 2 === 0) {
+    temp += "#"
+    console.log(temp);
+    }else{
+    temp += "X"
+    console.log(temp)
+    }
+        
+    }
